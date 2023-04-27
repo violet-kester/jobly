b8f22e794e7db90f4e6b131bc8ab7152bf258714
Vaughn Seekamp <vseekamp@gmail.com>
2023-04-27 13:27:46 -0700

    began work on user context/state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import RoutesList from './RoutesList';
+import {useState} from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from './NavBar';
+import userContext from "./userContext";
 
-/** App for Jobly!
+/** App for Jobly! TODO: UPDATE ME
  *
  * Props: N/A
  *
@@ -14,6 +16,30 @@ import NavBar from './NavBar';
  */
 
 function App() {
+
+  const [user, setUser] = useState({
+    user : {
+      applied: [],
+      username: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      isLoggedIn: false,
+      isAdmin: false
+      }
+    });
+    //TODO: this should be passed in as the default prefs state,
+    // which is then provided to Provider context
+    const defaultPref = {
+      user: {
+        username: '',
+        isLoggedIn: false,
+        isAdmin: false,
+        applied: []
+      }
+
+    }
+
   return (
     <div className="App">
       <BrowserRouter>
