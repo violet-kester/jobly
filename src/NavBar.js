4beba0f6fbceb46a890c9080564e25b4f6ce28a0
Violet Kester <kestervj89@gmail.com>
2023-08-03 00:37:56 -0400

    refactored navbar using material ui

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import userContext from "./userContext";
+import { Button, Stack, Typography } from '@mui/material';
 import "./NavBar.css";
 
 /** NavBar with links to main routes.
@@ -20,45 +21,28 @@ function NavBar({ logout }) {
   //TODO: loggedin navLinks could be apart of the same check
   return (
     <nav className="NavBar">
-      <div className="NavBar-logo">
-        <NavLink to="/">
-          Jobly
-        </NavLink>
-      </div>
-      <div className="NavBar-links">
 
-        {!localStorage.getItem("token") &&
-          <div className="NavBar-unprotected">
+      <NavLink to="/">
+        <Typography className="NavBar-logo" variant="h4">Jobly</Typography>
+      </NavLink>
 
-            <NavLink to="/login">
-              Login
-            </NavLink>
+      {/* unprotected routes */}
+      {!localStorage.getItem("token") &&
+        <Stack className="NavBar-unprotected" direction="row" spacing={1}>
+          <Button component={NavLink} to="/login" variant="outlined">Login</Button>
+          <Button component={NavLink} to="/signup" variant="outlined">Signup</Button>
+        </Stack>
+      }
 
-            <NavLink to="/signup">
-              Signup
-            </NavLink>
-          </div>
-        }
-        {localStorage.getItem("token") &&
-          <div className="NavBar-protected">
+      {/* protected routes */}
+      {localStorage.getItem("token") &&
+        <Stack className="NavBar-protected" direction="row" spacing={1}>
+          <Button component={NavLink} to="/companies" variant="outlined">Companies</Button>
+          <Button component={NavLink} to="/jobs" variant="outlined">Jobs</Button>
+          <Button onClick={logout} variant="outlined">Logout {user.user.username}</Button>
+        </Stack>
+      }
 
-            <NavLink to="/companies">
-              Companies
-            </NavLink>
-
-            <NavLink to="/jobs">
-              Jobs
-            </NavLink>
-
-            {/* <NavLink to="/profile">
-              Profile
-            </NavLink> */}
-
-            <button onClick={logout}>Logout {user.user.username}</button>
-
-          </div>
-        }
-      </div>
     </nav>
   );
 }
