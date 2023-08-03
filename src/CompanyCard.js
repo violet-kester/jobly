5f812c25720bfc052a357af37105c115be3a7f3b
Violet Kester <kestervj89@gmail.com>
2023-08-03 01:58:18 -0400

    refactored CompanyCard using material ui

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,5 +1,6 @@
 import "./CompanyCard.css";
 import { Link } from 'react-router-dom';
+import { Box, Card, CardContent, CardMedia, Typography, useTheme } from '@mui/material';
 
 /** CompanyCard component
  *
@@ -21,20 +22,52 @@ import { Link } from 'react-router-dom';
  */
 
 function CompanyCard({ company }) {
+  const theme = useTheme();
 
   return (
-    <div className="CompanyCard"  >
+    <Box className="CompanyCard">
       <Link to={`/companies/${company.handle}`}>
-        {company.logoUrl &&
+        <Card>
+          {company.logoUrl &&
+            <CardMedia
+              component="img"
+              image={company.logoUrl}
+              sx={{
+                height: 75,
+                marginTop: '10px',
+                objectFit: 'contain',
+              }}
+              m="3"
+            />
+          }
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{
+                color: (theme) => theme.palette.primary.dark
+              }}>
+              {company.name}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{
+                color: (theme) => theme.palette.primary.main
+              }}>
+              {company.description}
+            </Typography>
+          </CardContent>
+        </Card>
+        {/* {company.logoUrl &&
           <img
             className="CompanyCard-logo"
             src={company.logoUrl}
             alt={`${company.name} logo`} />
         }
         <h3 className="CompanyCard-name">{company.name}</h3>
-        <p>{company.description}</p>
+        <p>{company.description}</p> */}
       </Link>
-    </div>
+    </Box>
   );
 }
 
