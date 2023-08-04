7d51c2715986002a9aac0781f8f48a79cadd5619
Violet Kester <kestervj89@gmail.com>
2023-08-04 03:32:40 -0400

    refactoring nav bar using material ui

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,15 @@
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import userContext from "./userContext";
-import { Button, Stack, Typography } from '@mui/material';
-import "./NavBar.css";
-
+import {
+  AppBar,
+  Box,
+  Button,
+  Stack,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import FingerprintIcon from '@mui/icons-material/Fingerprint';
 /** NavBar with links to main routes.
  *
  * Props:
@@ -15,39 +21,66 @@ import "./NavBar.css";
  */
 
 function NavBar({ logout }) {
-  //TODO: user could be destructured problem
+  //TODO: user could be destructured
   const user = useContext(userContext);
-  // console.log("NavBar user=", user);
-  //TODO: loggedin navLinks could be apart of the same check
+
   return (
-    <nav className="NavBar">
+    <AppBar position="sticky" sx={{
+      backgroundColor: 'rgba(255, 255, 255, 0.5)',
+      backdropFilter: 'blur(10px)',
+      marginBottom: '20px',
+      boxShadow: '0px 3px 10px 3px rgba(0,0,0,0.1)',
+    }}>
+      <Toolbar sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '10px',
+      }}>
 
-      <NavLink to="/">
-        <Typography className="NavBar-logo" variant="h4" sx={{
-          color: (theme) => theme.palette.primary.dark,
-        }}>
-          Jobly
-        </Typography>
-      </NavLink>
+        {/* logo */}
 
-      {/* unprotected routes */}
-      {!localStorage.getItem("token") &&
-        <Stack className="NavBar-unprotected" direction="row" spacing={1}>
-          <Button component={NavLink} to="/login" variant="outlined">Login</Button>
-          <Button component={NavLink} to="/signup" variant="outlined">Signup</Button>
-        </Stack>
-      }
+        <NavLink to="/">
+          <Box sx={{
+            display: 'flex',
+            alignItems: 'center',
+            color: (theme) => theme.palette.primary.main
+          }}>
+            <Box
+              component="img"
+              sx={{ height: "35px"}}
+              alt="Logo"
+              src="../logo.png"
+            />
+            <Typography variant="h4" component="div" sx={{
+              marginLeft: '10px',
+              letterSpacing: '3px'
+            }}>
+              Jobly
+            </Typography>
+          </Box>
+        </NavLink>
 
-      {/* protected routes */}
-      {localStorage.getItem("token") &&
-        <Stack className="NavBar-protected" direction="row" spacing={1}>
-          <Button component={NavLink} to="/companies" variant="outlined">Companies</Button>
-          <Button component={NavLink} to="/jobs" variant="outlined">Jobs</Button>
-          <Button onClick={logout} variant="outlined">Logout {user.user.username}</Button>
-        </Stack>
-      }
+        {/* unprotected routes */}
 
-    </nav>
+        {!localStorage.getItem("token") &&
+          <Stack direction="row" spacing={1}>
+            <Button component={NavLink} to="/login" variant="outlined">Login</Button>
+            <Button component={NavLink} to="/signup" variant="outlined">Signup</Button>
+          </Stack>
+        }
+
+        {/* protected routes */}
+
+        {localStorage.getItem("token") &&
+          <Stack direction="row" spacing={1}>
+            <Button component={NavLink} to="/companies" variant="outlined">Companies</Button>
+            <Button component={NavLink} to="/jobs" variant="outlined">Jobs</Button>
+            <Button onClick={logout} variant="outlined">Logout {user.user.username}</Button>
+          </Stack>
+        }
+
+      </Toolbar>
+    </AppBar>
   );
 }
 
