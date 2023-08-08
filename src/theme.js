704dec5e3a338d40b28e131778473390399fbe4c
Violet Kester <kestervj89@gmail.com>
2023-08-07 21:14:34 -0400

    removed redundant code around styles

--- src/theme.js
+++ src/theme.js
@@ -3,58 +3,15 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     primary: {
-      light: '#fff',
-      main: '#333',
-      dark: '#262626',
+      light: '#fff', // white
+      main: '#333',  // gray
+      dark: '#262626', // dark gray
     },
     secondary: {
-      main: '#a5ebc8',
+      main: '#a5ebc8', // mint
     },
     tertiary: {
-      main: '#eeab63'
-    },
-  },
-  components: {
-    MuiCard: {
-      defaultProps: {
-        elevation: 0,
-      },
-    },
-    MuiContainer: {
-      styleOverrides: {
-        root: {
-          '@media (min-width: 600px)': {
-            padding: '0',
-          },
-        },
-      },
-    },
-    MuiTypography: {
-      styleOverrides: {
-        root: {
-          letterSpacing: '1px',
-        },
-        h1: {
-          color: '#333',
-          letterSpacing: '4px'
-        },
-        h4: {
-          color: '#333',
-        },
-        h5: {
-          color: '#333'
-        },
-      },
-    },
-    MuiButton: {
-      styleOverrides: {
-        contained: {
-          letterSpacing: '2px',
-        },
-        outlined: {
-          letterSpacing: '2px',
-        },
-      },
+      main: '#eeab63' // orange
     },
   },
 });
